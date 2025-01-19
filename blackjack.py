547ff4f70a5a6ae22fbe3a21c886a8f75a5bbf24
bogdan_bratu <bogdan_bratul@yahoo.com>
2025-01-19 15:06:31 +0200

    d

--- blackjack.py
+++ blackjack.py
@@ -49,14 +49,20 @@ class Player():
         else:
             self.bet = None
 
-    def __str__(self, newline=True) -> str:
+    def __str__(self, card, newline, hidden) -> str:
         s = ''
-        for card in self.hand:
-            if newline:
-                s += '\n' + str(card)
-            else:
-                s += str(card)
+        if hidden:
+            s= '\nHidden card'
+        elif newline:
+            s += '\n' + card.__str__()
+        else:
+            s = card.__str__()
         return s
+    
+    def show_hand(self, newline=True, hidden=False):
+        for card in self.hand:
+            print(self.__str__(card, newline, hidden))
+        print(self.hand_value)
     
     def draw_card(self, deck):
         deck = deck.deck
@@ -66,12 +72,9 @@ class Player():
         self.hand.append(card)
         self.hand_value += values[card.rank]
 
-    def show_hand(self, newline=True):
-        print(self.__str__(newline))
-        print(self.hand_value)
-
-    # def show_hand_value(self):
-    #     print(self.hand_value)
+    def draw_cards(self, deck):
+        for i in range(2):
+            self.draw_card(deck)
 
     def ask_for_card(self, deck):
         end = 0
@@ -81,7 +84,6 @@ class Player():
                 self.draw_card(deck)
                 self.show_hand()
                 self.adjust_for_ace()
-                self.show_hand_value()
                 end = self.check_hand()
                 if end:
                     self.set_ended_game()
@@ -93,22 +95,14 @@ class Player():
             self.hand_value -= 10
 
     def check_hand(self):
-        end = 0
+        blackjack, busted = 0, 0
         if self.hand_value == 21:
             print('Congratulations! Blackjack')
-            end = 1
-            if type(self) is Player:
-                self.chips += self.bet
-                print(f"Player has {self.chips} chips")
+            blackjack = 1
         elif self.hand_value > 21:
             print('Busted')
-            end = 1
-            if type(self) is Player:
-                self.chips -= self.bet
-                print(f"Player has {self.chips} chips left")
-            #elif type(self) is Dealer:
-            #    print(f'Dealer has busted! Player wins ')
-        return end
+            busted = 1
+        return blackjack, busted
     
     def compare_hands(self, dealer_hand_value):
         if self.hand_value > dealer_hand_value:
@@ -129,18 +123,30 @@ class Player():
     def set_ended_game(self):
         self.ended_game = True
         return self
+    
+    def player_won(self):
+        print('Congratulations! You won!')
+        self.chips += self.bet
+
 
 class Dealer(Player):
     def __init__(self) -> None:
         super().__init__()
 
+    def draw_card(self, deck):
+        return super().draw_card(deck)
+    
+    def draw_cards(self, deck):
+        return super().draw_cards(deck)
+
     def show_hand(self, hidden=False):
-        if hidden:
-            print("\nHidden card", end="")
-        return super().show_hand()
+        return super().show_hand(hidden=True)
     
     def get_hand_value(self):
         return self.hand_value
+    
+    def check_hand(self):
+        return super().check_hand()
 
 
 suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
@@ -165,71 +171,50 @@ def main():
 
     while True:
         print('\nStart of the game')
-        #list_of_ends = [False for player in players]
         for i, player in enumerate(players):
             player_no = i + 1
             print(f'\n\nPlayer {player_no}')
             player_bets[player_no] = player.get_bet()
-            for _ in range(2):
-                player.draw_card(deck)
+            player.draw_cards(deck)
             player.show_hand()
-
-        return 0 
-        for ind, player in enumerate(players):
-            print(f'\n\nPlayer {ind+1}')
-            player : Player = player
-            player.get_bet()
-            for _ in range(2):
-                player.draw_card(deck)
-            player.show_hand()
-            #player.show_hand_value()
-            end = player.check_hand()
-            if end:
-                player.set_ended_game()
-            if player.get_ended_game():
-                list_of_ends[ind] = True
+            player_blackjack, player_busted = player.check_hand()
+        #return 0
+        dealer = Dealer()
+        print(f'\n\nDealer')
+        dealer.draw_cards(deck)
+        dealer.show_hand()
+        dealer_blacjack, dealer_busted = dealer.check_hand()
         
-        if not all(list_of_ends):
-            dealer = Dealer()
-            print(f'\n\nDealer')
+        return 0
+        if dealer_busted:
+            player.player_won()
+        elif dealer_blacjack:
+            pass
+
+        for ind, player in enumerate(players):
+            if not player.get_ended_game():
+                print(f'\n\nPlayer {ind+1}')
+                player.show_hand()
+                player.ask_for_card(deck)
+
+        print(f'\n\nDealer')
+        dealer.show_hand()
+        while dealer.get_hand_value() < 17:
+            print('\nDealer draws')
             dealer.draw_card(deck)
-            dealer.show_hand(hidden=True)
-            dealer.draw_card(deck)
+            dealer.show_hand()
+            dealer.adjust_for_ace()
             end = dealer.check_hand()
             if end:
                 dealer.set_ended_game()
 
-            for ind, player in enumerate(players):
-                player : Player = player
-                if dealer.get_ended_game():
-                    continue
-                if not player.get_ended_game():
-                    print(f'\n\nPlayer {ind+1}')
-                    player.show_hand()
-                    player.show_hand_value()
-                    player.ask_for_card(deck)
-
-            print(f'\n\nDealer')
-            dealer.show_hand()
-            dealer.show_hand_value()
-            while dealer.get_hand_value() < 17:
-                print('\nDealer draws')
-                dealer.draw_card(deck)
-                dealer.show_hand()
-                dealer.adjust_for_ace()
-                dealer.show_hand_value()
-                end = dealer.check_hand()
-                if end:
-                    dealer.set_ended_game()
-
-            for ind, player in enumerate(players):
-                if dealer.get_ended_game() or player.get_ended_game():
-                    continue
-                print(f'\nPlayer {ind+1}')
-                player.show_hand()
-                player.show_hand_value()
-                player.compare_hands(dealer.get_hand_value())
-            print("\nEnd of the game")
+        for ind, player in enumerate(players):
+            if dealer.get_ended_game() or player.get_ended_game():
+                continue
+            print(f'\nPlayer {ind+1}')
+            player.show_hand()
+            player.compare_hands(dealer.get_hand_value())
+        print("\nEnd of the game")
 
         response = input('\nDo you want to play another game? y or n: ')
         if response == 'y':
